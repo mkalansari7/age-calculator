e8394f2614a67a4dda99031f310ec8c1fce558e4
mkalansari7 <mk.alansari7@gmail.com>
2022-04-08 03:18:33 +0300

    Done

--- age_calculator.py
+++ age_calculator.py
@@ -4,9 +4,9 @@ from datetime import date
 
 def get_dob():
     # write code here
-	year = input("Enter year of birth")
-	month = input("Enter month of birth")
-	day = input("Enter day of birth")
+	year = input("Enter year of birth: ")
+	month = input("Enter month of birth: ")
+	day = input("Enter day of birth: ")
 	return date(int(year), int(month), int(day))
 
 
@@ -20,18 +20,14 @@ def get_age(dob):
 def main():
 	# write code here
 	dob = get_dob()
-	print(dob)
 	if dob == None:
 		return
 	else:
 		age = get_age(dob)
-		if age < 0:
+		if age <= 0:
 			print("You should choose a valid date of birth")
-			return
 		else:
 			print(f"Your age is {age}")
-			return
-
 
 if __name__ == '__main__':
     main()
